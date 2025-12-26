58fe9c45a47a481fa1851b7e8de3502c182677da
mote1995 <dhzbill@gmail.com>
2025-12-27 00:38:52 +0800

    Fix gesture recognition by updating MediaPipe model URL

--- src/components/HandTracker.jsx
+++ src/components/HandTracker.jsx
@@ -17,7 +17,7 @@ export default function HandTracker() {
         "https://fastly.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.8/wasm"
       ];
 
-      const modelUrl = "https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker/full/1/hand_landmarker.task";
+      const modelUrl = "https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker/float16/1/hand_landmarker.task";
 
       for (const root of wasmRoots) {
         try {
