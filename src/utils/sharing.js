eb84698cb5b324bc988f7c1dcc5388c92eb81714
mote1995 <dhzbill@gmail.com>
2025-12-28 21:00:04 +0800

    fix: update NAS port to 32769 based on docker ps output

--- src/utils/sharing.js
+++ src/utils/sharing.js
@@ -6,7 +6,7 @@ const IMGBB_API_KEY = '6d207e02197a3d40d4094d1a2932a97f'; // Public test key
 
 // Configure your NAS URL here. Example: 'http://192.168.1.100:3001'
 // If empty, it will use cloud fallbacks (ImgBB/JsonBlob)
-export const NAS_URL = 'http://10.1.191.185:3001'; 
+export const NAS_URL = 'http://10.1.191.185:32769'; 
 
 /**
  * Uploads a file or data URL 
